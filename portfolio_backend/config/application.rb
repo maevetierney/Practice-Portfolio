958bd4e6398aafc5ffcee32a3bfd22de1d7d0624
Maeve Tierney <maevet27@gmail.com>
2016-07-26 16:44:59 -0700

    Testing CloudMailin

--- portfolio_backend/config/application.rb
+++ portfolio_backend/config/application.rb
@@ -19,19 +19,7 @@ Bundler.require(*Rails.groups)
 module PortfolioBackend
   class Application < Rails::Application
 
-      config.action_mailer.smtp_settings = {
-        address: "smtp.gmail.com",
-        port: 587,
-        domain: "<example.tld>",
-        user_name: "<username>",
-        password: "<password>",
-        authentication: :plain,
-        enable_starttls_auto: true
-      }
 
-      config.action_mailer.default_url_options = {
-        host: "yourdomain.tld"
-      }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
